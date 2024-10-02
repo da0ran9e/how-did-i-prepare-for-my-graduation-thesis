d504486c28f84994e0c2a053fbc148c86bb3dbf8
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2024-10-02 19:05:14 +0700

    [10/02/2024] adding parameters to the tictoc4 project

---
+++ 01. Introduction to OMNeT++/source/tictoc-tutorial1/out/clang-release/txc4.o.d
@@ -0,0 +1,4 @@
+out/clang-release//txc4.o: txc4.cc \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/include/omnetpp.h
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/include/omnetpp.h:
