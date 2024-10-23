38d4e9c97731106dac7f917e47d1301c073fa676
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2024-10-23 10:03:00 +0700

    [10/22/2024] simulation Dijktra algorithm

--- 01. Introduction to OMNeT++/source/tictoc-tutorial1/txc1.cc
+++ 01. Introduction to OMNeT++/source/tictoc-tutorial1/txc1.cc
@@ -3,42 +3,55 @@
 
  using namespace omnetpp;
 
- /**
-  * Derive the Txc1 class from cSimpleModule. In the Tictoc1 network,
-  * both the `tic' and `toc' modules are Txc1 objects, created by OMNeT++
-  * at the beginning of the simulation.
-  */
+ bool I = false;
+
  class Txc1 : public cSimpleModule
  {
    protected:
-     // The following redefined virtual function holds the algorithm.
      virtual void initialize() override;
      virtual void handleMessage(cMessage *msg) override;
+   private:
+           void createNode(const std::string& nodeName, int depth);
+           int maxDepth;
  };
 
- // The module class needs to be registered with OMNeT++
  Define_Module(Txc1);
 
  void Txc1::initialize()
  {
-     // Initialize is called at the beginning of the simulation.
-     // To bootstrap the tic-toc-tic-toc process, one of the modules needs
-     // to send the first message. Let this be `tic'.
-
-     // Am I Tic or Toc?
      if (strcmp("tic", getName()) == 0) {
-         // create and send first message on gate "out". "tictocMsg" is an
-         // arbitrary string which will be the name of the message object.
          cMessage *msg = new cMessage("tictocMsg");
          send(msg, "out");
      }
+     createNode("node1", 0);
  }
 
  void Txc1::handleMessage(cMessage *msg)
  {
-     // The handleMessage() method is called whenever a message arrives
-     // at the module. Here, we just send it to the other module, through
-     // gate `out'. Because both `tic' and `toc' does the same, the message
-     // will bounce between the two.
-     send(msg, "out"); // send out the message
+     send(msg, "out");
  }
+
+ void Txc1::createNode(const std::string& nodeName, int depth)
+ {
+     if (depth >= maxDepth) return; // Base case to stop recursion
+
+     // Create a node dynamically
+     cModuleType *nodeType = cModuleType::get("Txc1");
+     cModule *node = nodeType->create(nodeName.c_str(), this);
+
+     // Set up the node
+     node->finalizeParameters();
+     node->buildInside();
+     node->callInitialize();
+     //node->scheduleStart(simTime() + 3);
+
+     // Recursively create the next node
+     std::string nextNodeName = nodeName + std::to_string(depth + 1);
+     createNode(nextNodeName, depth + 1);
+
+//     // Optionally connect gates between nodes
+//     cGate *outGate = node->gate("out");
+//     //cGate *nextInGate = node->getParentModule()->getModuleByRelativePath(nextNodeName.c_str())->gate("in");
+//     outGate->connectTo(cModuleType::get("Txc1")->gate("out"));
+ }
+
