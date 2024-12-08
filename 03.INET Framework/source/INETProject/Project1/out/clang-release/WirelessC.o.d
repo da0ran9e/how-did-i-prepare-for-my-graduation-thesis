18859f62b08f4abad87206ae08671e061db8daa5
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2024-12-08 22:16:32 +0700

    [12/08/2024] inet framework project, sensor data collector

---
+++ 03.INET Framework/source/INETProject/Project1/out/clang-release/WirelessC.o.d
@@ -0,0 +1,28 @@
+out/clang-release//WirelessC.o: WirelessC.cc \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/ModuleAccess.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/INETDefs.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/precompiled_release.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/precompiled.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/include/omnetpp.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/opp_defines.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/features.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/Compat.h \
+  C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/InitStages.h
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/ModuleAccess.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/INETDefs.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/precompiled_release.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/precompiled.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/include/omnetpp.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/opp_defines.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/features.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/Compat.h:
+
+C:/Users/vuduc/OneDrive/Documents/GitHub/IoTOMNetpp/omnetpp-6.0.3/samples/inet4.3/src/inet/common/InitStages.h:
