2c4d58113d585748e81b98163979cbc944961b95
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2025-12-09 16:51:37 +0700

    Update wsn-routing.h

--- 09.ns-3-dev-git-ns-3.46/ns-3-dev-git-ns-3.46/src/wsn/model/wsn-routing.h
+++ 09.ns-3-dev-git-ns-3.46/ns-3-dev-git-ns-3.46/src/wsn/model/wsn-routing.h
@@ -1,12 +1,15 @@
-#pragma once
 #ifndef WSN_ROUTING_H
 #define WSN_ROUTING_H
 
 #include "ns3/object.h"
 #include "ns3/net-device.h"
+#include "ns3/packet.h"
 #include "ns3/mac16-address.h"
 #include "ns3/callback.h"
 #include "ns3/event-id.h"
+#include "ns3/nstime.h"
+#include "ns3/simulator.h"
+#include "ns3/node.h"
 
 namespace ns3 {
 namespace wsn {
@@ -19,16 +22,22 @@ public:
     WsnRoutingProtocol();
     virtual ~WsnRoutingProtocol() override;
     virtual void DoDispose() override;
-    
+
     void SetNetDevice(Ptr<NetDevice> dev);
     void Start();
 
 private:
     void SendBeacon();
-    bool ReceiveFromMac(Ptr<NetDevice> dev, Ptr<const Packet> p, uint16_t protocol,
-                        const Address &from, const Address &to);
+
+    bool ReceiveFromMac(Ptr<NetDevice> dev,
+                        Ptr<const Packet> p,
+                        uint16_t protocol,
+                        const Address &from);
+
+private:
     Ptr<NetDevice> m_dev;
     EventId m_beaconEvent;
+
     Time m_initialBeaconTime = Seconds(1.0);
     uint32_t m_beaconSize = 10;
 };
@@ -36,4 +45,4 @@ private:
 } // namespace wsn
 } // namespace ns3
 
-#endif // WSN_ROUTING_H
+#endif
